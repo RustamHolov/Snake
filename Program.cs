b0664381cf5a06ee247448b5f00b585c4963f406
RustamHolov <roshfor22@gmail.com>
2025-04-11 08:55:27 +0200

    scratch to render a field

--- Program.cs
+++ Program.cs
@@ -1,9 +1,10 @@
 ﻿namespace Snake;
-
+using System.Text;
 class Program
 {
     static void Main(string[] args)
     {
+        Console.OutputEncoding = Encoding.UTF8;
         View view = new View();
         view.Display();
     }
--- View.cs
+++ View.cs
@@ -3,32 +3,79 @@ using System.Text;
 public class View
 {
 
-    public static string DisplayField()
+    public static string BuildField(int width = 50, int height = 20, (int x, int y)? coords = null, string? insert = null, char symbol = ' ')
     {
-        string[,] buttons = new string[40,20];
+        char _symbol = symbol;
         StringBuilder buffered = new StringBuilder();
-        int rows = buttons.GetLength(0);
-        int cols = buttons.GetLength(1);
-        
+          //height |   y
+          //width  -   x
 
         // Build the top border
-        string topBorder = "┌" + new string('─', rows ) + "┐" + "\n";
+        string topBorder ="▄" + new string('▄', width ) + "▄" + "\n";
+        string[]? insertList = null;
         
         buffered.Append(topBorder);
+        if (insert != null){
+            insertList = insert.Split('\n');
+            if(coords == null){
+                coords = ((width - insertList[0].Length) / 2,(height - insertList.Length) / 2);
+            }
+        }
 
-        // Build the rows
-        for (int i = 0; i < cols; i++)
+
+        // Build the border and content
+        for (int i = 0; i < height; i++) // |
         {
-            buffered.Append("│" + new string(' ', rows ) + "│" + "\n");
+
+            string FillContent(string? content = null)
+            {
+                string tempContent = "";
+                for (int j = 0; j < width; j++) // - 
+                {
+                    if(insertList != null && content != null && coords.HasValue && j >= coords.Value.x && j < coords.Value.x + insertList[0].Length){
+                        tempContent += content;
+                        j += content.Length - 1;
+                    }else{
+                        tempContent += _symbol;
+                    }
+                }
+                return tempContent;
+            }
+            
+            string leftSide = "█";
+            string rightSide = "█";
+            string content;
+            if(insertList!= null && coords.HasValue && i >= coords.Value.y && i < coords.Value.y + insertList.Length) {
+                content = FillContent(content: insertList[i + insertList.Length - coords.Value.y - insertList.Length]);
+            }else{
+                content = FillContent();
+            }
+            buffered.Append(leftSide + content + rightSide + "\n");
         }
 
         // Build the bottom border
-        string bottomBorder = "└" + new string('─', rows ) + "┘";
+        string bottomBorder = "▀" + new string('▀', width ) + "▀";
         buffered.Append(bottomBorder);
         return buffered.ToString();
     }
     public void Display(){
-        Console.WriteLine(DisplayField());
+        
+        while(true){
+            Console.Clear();
+            int x = 0;
+            int y = 0;
+            Console.WriteLine(BuildField(insert: "N\nI\nG\nG\nA", coords: (x, y)));
+            ConsoleKey key;
+            key = Console.ReadKey().Key;
+            if (key == ConsoleKey.D){
+                x++;
+
+            }
+            if(key == ConsoleKey.S){
+                y++;
+            }
+        }
+        
     }
 
 }
